f8c9cd9972e29f8c0c8c60ab2731cd7ea93431c8
mahamnasimdurrani699 <mahamdurrani168@gmail.com>
2025-11-29 06:33:27 -0800

    update code in offer section

--- frontend/src/components/others/Card.jsx
+++ frontend/src/components/others/Card.jsx
@@ -57,8 +57,7 @@ function Card({ title, description, benefits = [], imageUrl }) {
         <button
   className="w-full py-3 rounded-xl font-semibold text-lg
              bg-gradient-to-r from-blue-500 to-blue-700
-             shadow-[0_0_15px_rgba(3,114,250,0.7)]
-             hover:shadow-[0_0_25px_rgba(3,114,250,1)]
+             shadow-[0_0_15px_rgba(3,114,250,0.7)
              transition duration-300 mt-auto"
 >
   Automation & Digital Transformation
